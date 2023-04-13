6e62d3ecfd713318abf889c22ccdf30e6384ef19
shahid <princesamofficial@gmail.com>
2023-04-13 21:45:49 +0530

    Testing

---
+++ script/login.js
@@ -0,0 +1,29 @@
+let username, password;
+const loginForm = document.querySelector("#login-form");
+const errorDiv = document.querySelector(".error-message");
+
+loginForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+  
+  const username = document.querySelector("#username").value;
+  const password = document.querySelector("#password").value;
+  
+  // Check validation
+  if (username === "admin" && password === "Admin@123") {
+    // Store credentials in local storage
+    localStorage.setItem("username", username);
+    localStorage.setItem("password", password);
+    
+    // Redirect to Resume page
+    window.open("resume.html", "_self")
+  } else if(username!= "admin" && password!= "Admin@123"){
+    errorDiv.textContent = "Invalid username and password.";
+  }else if(username!= "admin"){
+    errorDiv.textContent = "Invalid username";
+  }else if(password!= "Admin@123"){
+    errorDiv.textContent = "Invalid password";
+  }
+});
+
+// Restrict user from going back to Login page
+window.history.forward();
